e4a204ce8535f108ba7e321c9c595c4f1ed93b37
shahadat-web-dev <155247969+shahadat-web-dev@users.noreply.github.com>
2026-07-20 09:17:29 +0600

    Project setup and daisyui added

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+     
+     <button className="btn btn-primary">Test BUtton</button>
+    </div>
+  );
+}
